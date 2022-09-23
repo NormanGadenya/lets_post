bd0c461e1597dea615623dc2c7d1f1ef189a7e13
Ngadenya <ngadenya@gmail.com>
2022-09-23 11:38:43 +0300

    Styled up the base template

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,38 @@ from django.shortcuts import render
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Norman',
+        'title': 'Jack and the bean stalk',
+        'date_published': '23-01-2022',
+        'content': 'The anakim'
+    },
+    {
+        'author': 'Peter',
+        'title': 'Pat and the bean stalk',
+        'date_published': '23-01-2022',
+        'content': 'The Emim'
+
+    },
+    {
+        'author': 'Mackay',
+        'title': 'Mackay and the bean stalk',
+        'date_published': '23-01-2022',
+        'content': 'The Rephaim'
+
+    }
+]
+
+
+
 def home(request):
-    return HttpResponse('<h1> Blog home </a>')
+    context = {
+        'posts': posts,
+        'title': 'books'
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')
