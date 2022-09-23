ac5e9f31f080b220cd79568c0c5de426f35680df
Ngadenya <ngadenya@gmail.com>
2022-09-23 13:55:33 +0300

    User registration app

--- blog/views.py
+++ blog/views.py
@@ -1,37 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Norman',
-        'title': 'Jack and the bean stalk',
-        'date_published': '23-01-2022',
-        'content': 'The anakim'
-    },
-    {
-        'author': 'Peter',
-        'title': 'Pat and the bean stalk',
-        'date_published': '23-01-2022',
-        'content': 'The Emim'
-
-    },
-    {
-        'author': 'Mackay',
-        'title': 'Mackay and the bean stalk',
-        'date_published': '23-01-2022',
-        'content': 'The Rephaim'
-
-    }
-]
-
-
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all()
+,
         'title': 'books'
     }
     return render(request, 'blog/home.html', context)
