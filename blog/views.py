60d1189338c01e357a07c141c4d902229a90e05b
Ngadenya <ngadenya@gmail.com>
2022-11-03 15:11:19 +0300

    Post create and update

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
 
@@ -29,3 +29,25 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    # inorder to require the user to login before creating  a post
+    # and the LoginRequired.. has to be at the end
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # set the author to the current logged in user before validating form
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    # inorder to require the user to login before creating  a post
+    # and the LoginRequired.. has to be at the end
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # set the author to the current logged in user before validating form
+        return super().form_valid(form)
